cb531076fcfb808ea029f298b89be647bacf1e42
Robert Romero <robert.e.romero@gmail.com>
2025-06-28 12:29:12 -0700

    Initialize project skeleton

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">SlurmCostManager</h1>
+      <p>Welcome to the frontend!</p>
+    </div>
+  );
+}
+
+export default App;
