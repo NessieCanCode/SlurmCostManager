277249048ebd4e75701075bcd47afd2045d6d442
Robert Romero <robert.e.romero@gmail.com>
2025-08-05 14:51:08 -0700

    Generate invoices and refresh rates

--- test/unit/billing_summary.test.py
+++ test/unit/billing_summary.test.py
@@ -30,6 +30,36 @@ class BillingSummaryTests(unittest.TestCase):
         self.assertEqual(summary['details'][0]['cost'], 0.2)
         self.assertEqual(summary['invoices'][0]['file'], 'inv1.pdf')
 
+    def test_export_summary_applies_overrides_and_discounts(self):
+        usage = {
+            '2024-02': {
+                'research': {
+                    'core_hours': 10.0,
+                    'users': {'u': {'core_hours': 10.0, 'jobs': {}}},
+                },
+                'education': {
+                    'core_hours': 10.0,
+                    'users': {'u': {'core_hours': 10.0, 'jobs': {}}},
+                },
+                'special': {
+                    'core_hours': 10.0,
+                    'users': {'u': {'core_hours': 10.0, 'jobs': {}}},
+                },
+            }
+        }
+        with mock.patch.object(
+            SlurmDB,
+            'aggregate_usage',
+            return_value=(usage, {'daily': {}, 'monthly': {}, 'yearly': {}}),
+        ), mock.patch.object(SlurmDB, 'fetch_invoices', return_value=[]):
+            db = SlurmDB()
+            summary = db.export_summary('2024-02-01', '2024-02-29')
+        costs = {d['account']: d['cost'] for d in summary['details']}
+        self.assertAlmostEqual(costs['research'], 0.1)
+        self.assertAlmostEqual(costs['education'], 0.1)
+        self.assertAlmostEqual(costs['special'], 0.23)
+        self.assertAlmostEqual(summary['summary']['total'], 0.43)
+
 
 if __name__ == '__main__':
     unittest.main()
